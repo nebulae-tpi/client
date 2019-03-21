1dbd5d15cbb9794c7c538ac9e54671461b3e0040
juansmolano <juansmolano@gmail.com>
2019-03-21 12:53:57 -0500

    apllying development/production deployment routines

---
+++ frontend/client/src/environments/environment.dev.ts
@@ -0,0 +1,25 @@
+export const environment = {
+  production: true,
+  keycloak: {
+    url: 'https://tpi-dev.nebulae.com.co/auth',
+    realm: 'TPI',
+    clientId: 'CLIENT-APP',
+    onLoad: 'check-sso',
+    checkLoginIframe: false,
+    enableBearerInterceptor: true,
+    bearerExcludedUrls: [
+      '/assets'
+    ]
+  },
+  api: {
+    gateway: {
+      graphql: {
+        httpEndPoint:
+          'https://tpi-dev.nebulae.com.co/api/client-gateway/graphql/http',
+        wsEndPoint: 'wss://tpi-dev.nebulae.com.co/api/client-gateway/graphql/ws',
+        graphiqlEndPoint:
+          'https://tpi-dev.nebulae.com.co/api/client-gateway/graphiql'
+      }
+    }
+  }
+};
