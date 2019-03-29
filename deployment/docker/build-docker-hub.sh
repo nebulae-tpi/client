62eb16f262dc243b05a9beb8db1db11203cc2e59
Jaime Zapata <esteban.zapata@nebulae.com.co>
2019-03-29 13:51:05 -0500

    Removed font import

--- deployment/docker/build-docker-hub.sh
+++ deployment/docker/build-docker-hub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DOCKERHUB_IMAGE=pwa-client
-export DOCKERHUB_TAG=1.0.23
+export DOCKERHUB_TAG=1.0.24
 
 rm -rf deployment/docker/dist/
 cp -R $FRONTEND_SHELL_PATH/dist deployment/docker/dist
