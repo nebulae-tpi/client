3d37c669a1cca40414a5141b2e418342fc339d23
Daniel Tobon <daniel.tobon@nebulae.com.co>
2019-03-29 10:59:24 -0500

    Se elimina elementos no necesatios creados por firebase

--- frontend/client/src/environments/environment.prod.ts
+++ frontend/client/src/environments/environment.prod.ts
@@ -1,30 +1,42 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
   production: true,
   keycloak: {
-    url: 'https://tpi.nebulae.com.co/auth',
+    url: 'https://tpi-dev.nebulae.com.co/auth',
     realm: 'TPI',
-    clientId: 'CLIENT-APP',
+    clientId: 'CLIENT-APP-LOCAL',
     onLoad: 'check-sso',
     checkLoginIframe: false,
     enableBearerInterceptor: true,
-    bearerExcludedUrls: [
-      '/assets'
-    ]
+    bearerExcludedUrls: ['/assets']
   },
   api: {
     gateway: {
       graphql: {
         httpEndPoint:
-          'https://tpi.nebulae.com.co/api/client-gateway/graphql/http',
-        wsEndPoint: 'wss://tpi.nebulae.com.co/api/client-gateway/graphql/ws',
+          'https://tpi-dev.nebulae.com.co/api/client-gateway/graphql/http',
+        wsEndPoint: 'wss://tpi-dev.nebulae.com.co/api/client-gateway/graphql/ws',
         graphiqlEndPoint:
-          'https://tpi.nebulae.com.co/api/client-gateway/graphiql'
+          'https://tpi-dev.nebulae.com.co/api/client-gateway/graphiql'
       }
     }
   },
   google: {
     maps: {
-      key: 'AIzaSyC1VkMKnBB_TATeaszTe_a8phyo-B8DSVg'
+      key: 'AIzaSyByGMKwZIYXqcPONjNSX-KHJ9kbP5tBu5I'
     }
   }
 };
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
