9c32bae7bc62c6c94bfbcf5be4e7db8df315dccf
Daniel Tobon <daniel.tobon@nebulae.com.co>
2019-04-01 16:56:21 -0500

    Se incrementa versión

--- deployment/docker/build-docker-hub.sh
+++ deployment/docker/build-docker-hub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DOCKERHUB_IMAGE=pwa-client
-export DOCKERHUB_TAG=1.0.29
+export DOCKERHUB_TAG=1.0.30
 
 rm -rf deployment/docker/dist/
 cp -R $FRONTEND_SHELL_PATH/dist deployment/docker/dist
