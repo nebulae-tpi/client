599ac8855410df5b027916fced1ab06e25a8e64d
juansantane <juan.santa@nebulae.com.co>
2019-12-18 17:56:00 -0500

    version updated

--- deployment/docker/build-docker-hub.sh
+++ deployment/docker/build-docker-hub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DOCKERHUB_IMAGE=pwa-client
-export DOCKERHUB_TAG=1.1.27
+export DOCKERHUB_TAG=1.1.28
 
 rm -rf deployment/docker/dist/
 cp -R $FRONTEND_SHELL_PATH/dist deployment/docker/dist
