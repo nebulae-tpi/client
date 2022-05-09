47aa8178ee02f81fc83186cf1f04157aebc0f040
Daniel Tobón Uribe <daniel.tobon@nebulae.com.co>
2022-05-09 15:37:01 -0500

    Se agregan files de assets en app mobiles

--- deployment/docker/build-docker-hub.sh
+++ deployment/docker/build-docker-hub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DOCKERHUB_IMAGE=pwa-client
-export DOCKERHUB_TAG=1.1.102
+export DOCKERHUB_TAG=1.1.103
 
 rm -rf deployment/docker/dist/
 cp -R $FRONTEND_SHELL_PATH/dist deployment/docker/dist
